961033bd862e9852d10dbc4a9620eb319a5c7933
amevertiche <ameyalli.contreras@vertiche.com.mx>
2026-09-15 15:22:03 -0600

    Vite config url smolbox

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ export default defineConfig({
     plugins: [react()],
 
     server: {
+        host: '0.0.0.0',
+        allowedHosts: [
+            'smolbox.vertiche.com.mx',
+        ],
         proxy: {
             "/api": {
                 target: "http://localhost:8000",
